121cbaaf9aac9f6f9587d6451bc576d486c4089c
JMJarvis1 <97367692+JMJarvis1@users.noreply.github.com>
2024-07-16 15:55:55 +0000

    Fix bugs affecting: 1. Game lost message 2. Clearing screen on non-windows systems

--- Spacegames/Starship_Takeoff.py
+++ Spacegames/Starship_Takeoff.py
@@ -1,29 +1,32 @@
 import os
+import platform
 import random
 
-os.system("cls")
+if platform.system == "Windows":
+    os.system("cls")
+else:
+     os.system("clear")
 
-print("STARSHIP TAKE-OFF")
 
 g = random.randint(1, 20)
 w = random.randint(1, 40)
 
 r = g * w
 
-print(f"GRAVITY= {g}\n")
+print("STARSHIP TAKE-OFF\n")
+print(f"GRAVITY = {g}\n")
 print("TYPE IN FORCE")
 
 for tries in range(10):
     force = int(input("> "))
-
+    
     if force > r:
-        print("TOO HIGH")
-        continue
+        print("TOO HIGH") 
     elif force < r:
         print("TOO LOW")
-        continue
     elif force == r:
         print("GOOD TAKE OFF")
         break
-    else:
-        print("YOU FAILED - \n THE ALIENS GOT YOU. ")
+    
+    if tries == 9:
+        print("YOU FAILED - \nTHE ALIENS GOT YOU. ")
